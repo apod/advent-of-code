7e27a9715e39f51e147910ceec120f33b5baf591
Alex Podaras <apodaras@gmail.com>
2015-12-23 01:38:32 +0200

    Finish up the solution for day 13

--- src/advent_of_code/day_13.clj
+++ src/advent_of_code/day_13.clj
@@ -1,6 +1,7 @@
 (ns advent-of-code.day-13
   (:require [clojure.java.io :as io]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.math.combinatorics :as combo]))
 
 (def input (slurp (io/resource "day-13/input.txt")))
 
@@ -13,7 +14,21 @@
 
 (def seatings (parse-seatings (str/split-lines input)))
 
+(defn people [seatings]
+  (set (flatten (keys seatings))))
+
+(defn neighbors [p]
+  (let [p (partition 2 1 (cons (last p) p))]
+    (concat p (map reverse p))))
+
+(defn optimal-change [seatings]
+  (->> (combo/permutations (people seatings))
+       (pmap (fn [coll] {:seating (neighbors coll)
+                         :happiness (reduce + (map (partial get seatings) (neighbors coll)))}))
+       (apply max-key :happiness)))
+
 (comment
+  ;; Sample
   (def sample
     (parse-seatings ["Alice would gain 54 happiness units by sitting next to Bob."
                      "Alice would lose 79 happiness units by sitting next to Carol."
@@ -28,8 +43,20 @@
                      "David would lose 7 happiness units by sitting next to Bob."
                      "David would gain 41 happiness units by sitting next to Carol."]))
 
-  (get sample ["Bob" "David"])
-  ;;=> -63
-  (get sample ["David" "Bob"])
-  ;;=> -7
+  (optimal-change sample)
+  ;;=> {:seating (("Alice" "David") ("David" "Carol") ("Carol" "Bob") ("Bob" "Alice") ("David" "Alice") ("Carol" "David") ("Bob" "Carol") ("Alice" "Bob")), :happiness 330}
+
+  ;; Part 1
+  (optimal-change seatings)
+  ;;=> {:seating (("George" "David") ("David" "Eric") ("Eric" "Carol") ("Carol" "Frank") ("Frank" "Bob") ("Bob" "Alice") ("Alice" "Mallory") ("Mallory" "George") ("David" "George") ("Eric" "David") ("Carol" "Eric") ("Frank" "Carol") ("Bob" "Frank") ("Alice" "Bob") ("Mallory" "Alice") ("George" "Mallory")), :happiness 733}
+
+  ;; Part 2
+  (def seatings-including-me
+    (into seatings
+          (mapcat (fn [p] [[["Alex" p] 0]
+                           [[p "Alex"] 0]]) (people seatings))))
+
+  (optimal-change seatings-including-me)
+  ;;=> {:seating (("George" "David") ("David" "Eric") ("Eric" "Carol") ("Carol" "Frank") ("Frank" "Bob") ("Bob" "Alice") ("Alice" "Mallory") ("Mallory" "Alex") ("Alex" "George") ("David" "George") ("Eric" "David") ("Carol" "Eric") ("Frank" "Carol") ("Bob" "Frank") ("Alice" "Bob") ("Mallory" "Alice") ("Alex" "Mallory") ("George" "Alex")), :happiness 725}
+
   )
