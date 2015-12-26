9266d19c30dfdfce948614c11acc1239b56d4352
Alex Podaras <apodaras@gmail.com>
2015-12-26 02:10:55 +0200

    Solve day 15 parts

--- src/advent_of_code/day_15.clj
+++ src/advent_of_code/day_15.clj
@@ -15,13 +15,45 @@
 
 (def ingredients (map parse-ingredient (str/split-lines input)))
 
-(comment
-  (first ingredients)
-  ;;=> {:capacity 5, :durability -1, :flavor 0, :texture 0, :calories 5}
+(defn score [combination properties ingredients]
+  (let [vals-by-property (map (fn [prop]
+                                (map prop ingredients))
+                              properties)]
+    (reduce * (map (fn [vals]
+                     (let [x (reduce + (map * vals combination))]
+                       (if (neg? x) 0 x))) vals-by-property))))
 
-  (frequencies (map (partial apply +) (for [a (range (inc 100))
-                                            b (range (- (inc 100) a))
-                                            c (range (- (inc 100) a b))]
-                                        [a b c (- 100 a b c)])))
-  ;;=> {100 176851}
+(def combinations
+  "176851 recipes."
+  (for [a (range (inc 100))
+        b (range (- (inc 100) a))
+        c (range (- (inc 100) a b))]
+    [a b c (- 100 a b c)]))
+
+(comment
+  ;; Sample
+  (def sample (map parse-ingredient
+                   ["Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8"
+                    "Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3"]))
+
+  (score [44 56] [:capacity :durability :flavor :texture] sample)
+  ;;=> 62842880
+
+  ;; Part 1
+  (apply (partial max-key :score)
+         (map (fn [c]
+                {:combination c
+                 :score (score c [:capacity :durability :flavor :texture] ingredients)})
+                combinations))
+  ;;=> {:combination [28 35 18 19], :score 13882464}
+
+  ;; Part 2
+  (apply (partial max-key :score)
+         (filter #(= (:calories %) 500)
+                 (map (fn [c]
+                        {:combination c
+                         :calories (reduce + (map * c (map :calories ingredients)))
+                         :score (score c [:capacity :durability :flavor :texture] ingredients)})
+                      combinations)))
+  ;;=> {:combination [27 27 15 31], :calories 500, :score 11171160}
   )
